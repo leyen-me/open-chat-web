8a4f193081e16083aaa8cd1a4953a0fc2a25668a
DIFFFFFFT <672228275@qq.com>
2024-04-01 12:43:44 +0800

    fix bug

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -100,14 +100,6 @@
             : ""
         }}
       </el-text>
-      <a href="#">
-        <span class="sr-only">Your profile</span>
-        <img
-          class="h-8 w-8 rounded-full bg-gray-50"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          alt=""
-        />
-      </a>
     </div>
 
     <main class="flex-1" :key>
